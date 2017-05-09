1969cd691a48bd8443c3635597a73354d3a1de1e
Tin Trung Dang <tindang1710@gmail.com>
2017-05-09 04:17:17 +0000

    make the request

--- download_avatars.js
+++ download_avatars.js
@@ -8,6 +8,9 @@ var request = require('request');
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+GITHUB_USER+':'+GITHUB_TOKEN +'@api.github.com/repos/'+repoOwner+'/'+repoName +'/contributors';
   console.log(requestURL)
+  request.get(requestURL)
 }
-
-getRepoContributors("jquery", "jquery")
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
