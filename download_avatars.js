1f9cf65f80181177ab04be5cc1927ffa31dca09f
Tin Trung Dang <tindang1710@gmail.com>
2017-05-09 03:58:06 +0000

    defined requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
+var GITHUB_USER = "tindang17";
+var GITHUB_TOKEN = "6a0520dbf4c3598b8a1c40620e913c22cd4b9731";
+
+
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+GITHUB_USER+':'+GITHUB_TOKEN +'@api.github.com/repos/'+repoOwner+'/'+repoName +'/contributors';
+  console.log(requestURL)
+}
+
+getRepoContributors("jquery", "jquery")
