b3cd131a014b8daccf7dcde98986ea04333548ad
Tin Trung Dang <tindang1710@gmail.com>
2017-05-09 19:49:44 +0000

    pictures are downloaded

--- download_avatars.js
+++ download_avatars.js
@@ -3,18 +3,19 @@ var GITHUB_TOKEN = "6a0520dbf4c3598b8a1c40620e913c22cd4b9731";
 
 
 var request = require('request');
-// var fs = require('fs')
+var fs = require('fs')
 
+//----------------------------------------------------------------//
 
 function getRepoContributors(repoOwner, repoName, cb) {
-var requestURL = 'https://'+GITHUB_USER+':'+GITHUB_TOKEN +'@api.github.com/repos/'+repoOwner+'/'+repoName +'/contributors';
+  var requestURL = 'https://'+GITHUB_USER+':'+GITHUB_TOKEN +'@api.github.com/repos/'+repoOwner+'/'+repoName +'/contributors';
 
-var obj = {
-  url: requestURL,
-  headers: {
-    'User-Agent': 'Lighthouse Project'
-  }
-};
+  var obj = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'Lighthouse Project'
+    }
+  };
 
 
 
@@ -22,29 +23,35 @@ var obj = {
     // if (err) throw err;
     var contributors = JSON.parse(body)
     contributors.forEach(function(item) {
-      console.log(item.avatar_url)
+      downloadImageByURL(item.avatar_url, `./avatars/${item.login}.jpg`)
     })
     // console.log(JSON.parse(body.avatar_url));
   });
 }
 
+//---------------------------------------------------------------//
 
-
-
-
-
-
-
-
-
-
-
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+        .on('error', function(err){
+          throw error;
+        })
+        .on('data', function(data){})
+        .pipe(fs.createWriteStream(filePath));
+  console.log('download is completed')
+}
 
 //---------------------------------------------------------------//
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+
+getRepoContributors("jquery", "jquery", function(err, result) {});
+
+
+
+
+
+
+
+
 
 // };
 
